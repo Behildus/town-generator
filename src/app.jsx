e676476e2bafba774e8635c2e8d9767d4e803dd1
Behildus <behildus@gmail.com>
2022-05-05 17:53:00 -0400

    Theming changing
    - App takes up full space
    - dark mode added

--- src/app.jsx
+++ src/app.jsx
@@ -4,10 +4,8 @@ import EditPeople from './components/edit-people.jsx';
 import EditItems from './components/edit-items.jsx';
 import GenerateTown from './components/generate-town.jsx';
 import PropTypes from 'prop-types';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
+import {Tab, Tabs, Typography, Box, Switch, Drawer} from '@mui/material';
+import { styled, ThemeProvider, createTheme } from '@mui/material/styles';
 
 function TabPanel(props) {
     const { children, value, index, ...other } = props;
@@ -42,36 +40,70 @@ function a11yProps(index) {
     };
 }
 
+const ColorModeContext = React.createContext({ toggleColorMode: () => {} });
+
 function App() {
     const [value, setValue] = React.useState(0);
+    const [mode, setMode] = React.useState('light');
+    const colorMode = React.useMemo(
+        () => ({
+            toggleColorMode: () => {
+            setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+            },
+        }),
+        [],
+    );
+      
+    const theme = React.useMemo(
+        () =>
+          createTheme({
+            palette: {
+              mode,
+            },
+          }),
+        [mode],
+    );
 
     const handleChange = (event, newValue) => {
         setValue(newValue);
-        };
+    };
+
 
     return (
-        <Box sx={{ width: '100%' }}>
-            <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-                <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
-                    <Tab label="Generate Town" {...a11yProps(0)} />
-                    <Tab label="Species" {...a11yProps(1)} />
-                    <Tab label="People" {...a11yProps(2)} />
-                    <Tab label="Items" {...a11yProps(3)} />
-                </Tabs>
-            </Box>
-            <TabPanel value={value} index={0}>
-                <GenerateTown/>
-            </TabPanel>
-            <TabPanel value={value} index={1}>
-                <EditSpecies/>
-            </TabPanel>
-            <TabPanel value={value} index={2}>
-                <EditPeople/>
-            </TabPanel>
-            <TabPanel value={value} index={3}>
-                <EditItems/>
-            </TabPanel>
-        </Box>
+        <ColorModeContext.Provider value={colorMode}>
+            <ThemeProvider theme={theme}>
+                <Box className='app' sx={{
+                    width: '100%',
+                    bgcolor: 'background.default',
+                    color: 'text.primary'
+                    }}
+                >
+                    <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                        <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+                            <Tab label="Generate Town" {...a11yProps(0)} />
+                            <Tab label="Species" {...a11yProps(1)} />
+                            <Tab label="People" {...a11yProps(2)} />
+                            <Tab label="Items" {...a11yProps(3)} />
+                        </Tabs>
+                    </Box>
+                    <TabPanel value={value} index={0}>
+                        <GenerateTown/>
+                    </TabPanel>
+                    <TabPanel value={value} index={1}>
+                        <EditSpecies/>
+                    </TabPanel>
+                    <TabPanel value={value} index={2}>
+                        <EditPeople/>
+                    </TabPanel>
+                    <TabPanel value={value} index={3}>
+                        <EditItems/>
+                    </TabPanel>
+                    <Switch onChange={colorMode.toggleColorMode}/>
+                </Box>
+            </ThemeProvider>
+        </ColorModeContext.Provider>
+        
+        
     );
 }
 
